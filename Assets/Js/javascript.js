facbeed1fd6ceb4f162b799f6a1c82f52c5ae34b
a e <myaref7@gmail.com>
2022-08-17 00:14:24 +0430

    add link to side bar

--- Assets/Js/javascript.js
+++ Assets/Js/javascript.js
@@ -13,3 +13,20 @@ function show() {
         }
     }
 }
+
+/* اکتیو شدن ایتم های منو */
+let navLinks = document.querySelectorAll('.nav-link');
+
+for (let i = navLinks.length; i--;) {
+    navLinks[i].addEventListener('click', eventHandler);
+}
+
+
+function eventHandler() {
+    this.classList.add('active');
+    this.classList.remove('link-dark');
+    for (let i = navLinks.length; i--;) {
+        if (navLinks[i] != this) navLinks[i].classList.remove('active')
+        if (navLinks[i] != this) navLinks[i].classList.add('link-dark')
+    }
+}
