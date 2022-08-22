21416efa1abbb66c94d544f639b27479e018e5a9
a e <myaref7@gmail.com>
2022-08-22 13:17:58 +0430

    change style btn-open --- add style scrolbar custom --- change coment uploadin file.js --- add part off/on gozarshs

--- Assets/Js/uploading file.js
+++ Assets/Js/uploading file.js
@@ -1,27 +1,28 @@
+/* مقدار دهی متغیر ها */
 var $fileInput = $('.file-input');
 var $droparea = $('.file-drop-area');
 
-// highlight drag area
+// فعال کردن باکس انتخاب فایل
 $fileInput.on('dragenter focus click', function () {
     $droparea.addClass('is-active');
 });
 
-// back to normal state
+// برگشتن به حالت اولیه
 $fileInput.on('dragleave blur drop', function () {
     $droparea.removeClass('is-active');
 });
 
-// change inner text
+// تغییر متن باکس به اسم فایل
 $fileInput.on('change', function () {
     var filesCount = $(this)[0].files.length;
     var $textContainer = $(this).prev();
 
     if (filesCount === 1) {
-        // if single file is selected, show file name
+        // اگر یک فایل انتخاب شده بود اسم همان فایل نمایش داده بشه
         var fileName = $(this).val().split('\\').pop();
         $textContainer.text(fileName);
     } else {
-        // otherwise show number of files
-        $textContainer.text(filesCount + ' files selected');
+        // اگر تعداد بیش تر از یک فایل بود تعداد فایل هارا نمایش دهد
+        $textContainer.text(filesCount + ' فایل انتخاب شده');
     }
 });
