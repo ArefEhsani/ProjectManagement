6e8e5fa6e8dba6f4ae558df652f9752e2e5dc200
a e <myaref7@gmail.com>
2022-09-09 23:39:48 +0430

    update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -26,9 +26,7 @@ urlpatterns = [
     path('students/', include("students.urls")),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404 = 'accounts.views.handler404'
